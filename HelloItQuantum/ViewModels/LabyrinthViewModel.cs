9cca4ea0e956239d05ff7413a8ffd2fd6f54f1a2
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-06-29 15:21:43 +0300

    start of work on  "code area"

--- HelloItQuantum/ViewModels/LabyrinthViewModel.cs
+++ HelloItQuantum/ViewModels/LabyrinthViewModel.cs
@@ -1,10 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using ReactiveUI;
 
 namespace HelloItQuantum.ViewModels
 {
 	public class LabyrinthViewModel : ReactiveObject
 	{
-	}
+        public ObservableCollection<MyItem> MyItems { get; } = new ObservableCollection<MyItem>();
+
+        public ICommand AddRowCommand { get; }
+
+        public LabyrinthViewModel()
+        {
+            AddRowCommand = ReactiveCommand.Create(AddRow);
+        }
+
+        private void AddRow()
+        {
+            MyItems.Add(new MyItem { MyProperty = $"Строка {MyItems.Count + 1}" });
+        }
+
+    }
+    public class MyItem
+    {
+        public string MyProperty { get; set; }
+    }
 }
