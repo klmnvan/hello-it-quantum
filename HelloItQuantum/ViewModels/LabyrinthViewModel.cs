c1269d153facd6a99c877138f961e80828e2c487
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-07-01 23:50:23 +0300

    fix: add function add TextBox in StackPanel for Labitinth play

--- HelloItQuantum/ViewModels/LabyrinthViewModel.cs
+++ HelloItQuantum/ViewModels/LabyrinthViewModel.cs
@@ -2,29 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Avalonia.Controls;
+using DynamicData;
 using ReactiveUI;
 
 namespace HelloItQuantum.ViewModels
 {
-    public class LabyrinthViewModel : ReactiveObject
+    public class LabyrinthViewModel : MainWindowViewModel
     {
-        private List<NameComand> listCommandForRobots = new List<NameComand>();
+
+        /*private List<NameComand> listCommandForRobots = new List<NameComand>();
 
         public List<NameComand> ListCommandForRobots { get => listCommandForRobots; set => listCommandForRobots = value; }
 
-        public void addRight() {
+        public void AddRight() {
             ListCommandForRobots.Add(new NameComand { name = "go right" });
 
+        }*/
+
+        StackPanel listCommandForRobots = new StackPanel();
+        public StackPanel ListCommandForRobots { get => listCommandForRobots; set => SetProperty(ref listCommandForRobots, value); }
+
+        public void AddRight()
+        {
+            TextBox tb = new TextBox();
+            tb.Text = "Прививи";
+            ListCommandForRobots.Children.Add(tb);
+
         }
-
-        
-
-
     }
 
     public class NameComand {
         public string name { get; set; }
-
     }
 
 }
