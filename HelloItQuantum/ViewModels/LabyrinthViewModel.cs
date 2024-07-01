f98c57ec2021e44bb6be05d6fabca3fea70bce15
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-07-01 23:32:07 +0300

    refactor: add right

--- HelloItQuantum/ViewModels/LabyrinthViewModel.cs
+++ HelloItQuantum/ViewModels/LabyrinthViewModel.cs
@@ -6,25 +6,25 @@ using ReactiveUI;
 
 namespace HelloItQuantum.ViewModels
 {
-	public class LabyrinthViewModel : ReactiveObject
-	{
-        public ObservableCollection<MyItem> MyItems { get; } = new ObservableCollection<MyItem>();
-
-        public ICommand AddRowCommand { get; }
-
-        public LabyrinthViewModel()
-        {
-            AddRowCommand = ReactiveCommand.Create(AddRow);
-        }
-
-        private void AddRow()
-        {
-            MyItems.Add(new MyItem { MyProperty = $"Строка {MyItems.Count + 1}" });
-        }
-
-    }
-    public class MyItem
+    public class LabyrinthViewModel : ReactiveObject
     {
-        public string MyProperty { get; set; }
+        private List<NameComand> listCommandForRobots = new List<NameComand>();
+
+        public List<NameComand> ListCommandForRobots { get => listCommandForRobots; set => listCommandForRobots = value; }
+
+        public void addRight() {
+            ListCommandForRobots.Add(new NameComand { name = "go right" });
+
+        }
+
+        
+
+
     }
+
+    public class NameComand {
+        public string name { get; set; }
+
+    }
+
 }
